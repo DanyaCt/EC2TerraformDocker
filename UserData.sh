fa97242ea18e1cc1168a083ef1e6b47beffd9356
DanyaCt <sebtec1212@gmail.com>
2023-01-27 01:26:30 +0200

    add monitoring disk and memory of EC2

--- UserData.sh
+++ UserData.sh
@@ -2,6 +2,40 @@
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo service docker start
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+newgrp docker
 sudo docker pull sebtec1212/myimage
 docker run -d --name watchtower --restart always -v /var/run/docker.sock:/var/run/docker.sock v2tec/watchtower -i 30
 sudo docker run -d -p 80:80 sebtec1212/myimage
+
+sudo yum install -y amazon-cloudwatch-agent
+sudo cat >> /opt/aws/amazon-cloudwatch-agent/bin/config.json <<EOF
+{
+    "agent": {
+            "metrics_collection_interval": 60,
+            "run_as_user": "root"
+    },
+    "metrics": {
+            "metrics_collected": {
+                    "disk": {
+                            "measurement": [
+                                    "used_percent"
+                            ],
+                            "metrics_collection_interval": 60,
+                            "resources": [
+                                    "/"
+                            ]
+                    },
+                    "mem": {
+                            "measurement": [
+                                    "mem_used_percent"
+                            ],
+                            "metrics_collection_interval": 60
+                    }
+            }
+    }
+}
+EOF
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json
+
--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "aws_instance" "web-server-instance" {
   ami               = data.aws_ami.amazon-linux-2.id
   instance_type     = "t2.micro"
   key_name          = "MyKey"
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   vpc_security_group_ids = [aws_security_group.allow_web.id]
   associate_public_ip_address = true
   user_data = file("UserData.sh")
@@ -90,4 +91,67 @@ resource "aws_cloudwatch_metric_alarm" "MyAlarm" {
 
 output "instance_ip" {
   value = aws_instance.web-server-instance.public_ip
+}
+
+resource "aws_iam_role" "role" {
+  name = "test-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : "sts:AssumeRole",
+          "Principal" : {
+            "Service" : "ec2.amazonaws.com"
+          },
+          "Effect" : "Allow"
+        }
+      ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:PutMetricData",
+                "ec2:DescribeVolumes",
+                "ec2:DescribeTags",
+                "logs:PutLogEvents",
+                "logs:DescribeLogStreams",
+                "logs:DescribeLogGroups",
+                "logs:CreateLogStream",
+                "logs:CreateLogGroup"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ssm:GetParameter"
+            ],
+            "Resource": "arn:aws:ssm:*:*:parameter/AmazonCloudWatch-*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.role.name
 }
